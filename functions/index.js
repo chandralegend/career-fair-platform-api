dfdeca17c3700b0d833e880e4c74e4d3cf059781
Chandra Irugalbandara <irugalbandarachandra@gmail.com>
2021-06-19 23:04:18 +0530

    Update index.js

--- functions/index.js
+++ functions/index.js
@@ -15,11 +15,11 @@ const app = express();
 app.use(cors({ origin: true }));
 
 //STUDENT
-app.post("/api/v1/student", createStudent);
-app.get("/api/v1/student/:id", getStudent);
-app.put("/api/v1/student/updatecv/:id", updateStudentCV);
-app.put("/api/v1/student/updateprofilepic/:id", updateStudentPhoto);
-app.put("/api/v1/student/updateprioritylist/:id", updateCompanyPriority);
-app.get("/api/v1/company/:id", getCompany);
+app.post("/v1/student", createStudent);
+app.get("/v1/student/:id", getStudent);
+app.put("/v1/student/updatecv/:id", updateStudentCV);
+app.put("/v1/student/updateprofilepic/:id", updateStudentPhoto);
+app.put("/v1/student/updateprioritylist/:id", updateCompanyPriority);
+app.get("/v1/company/:id", getCompany);
 
-exports.app = functions.https.onRequest(app);
+exports.api = functions.https.onRequest(app);
